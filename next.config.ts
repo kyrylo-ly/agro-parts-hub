0c5fc166b75a5be35547fbfcae691c53254cb96f
Kyrylo <kyrylo.lytvishko@gmail.com>
2026-08-22 17:33:50 +0300

    chore: next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-a63d741996974f309ae0fc2488e0ba5b.r2.dev",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
