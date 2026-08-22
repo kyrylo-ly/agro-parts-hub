941b71a146150f9071a5dacca3dbcbbdc99ad2cb
Kyrylo <kyrylo.lytvishko@gmail.com>
2026-08-22 23:25:53 +0300

    chore: SpeedInsights [skip ci]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
@@ -91,6 +92,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         <JsonLd />
         {children}
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
